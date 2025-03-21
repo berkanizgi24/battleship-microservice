6754b84a050d83705bba309fb9697b8c6486f6cc
Berkan Izgi <biz3926@students.fhv.at>
2025-03-21 14:37:22 +0100

    yessir

--- battleship_microservices/battleship_berkan_izgi/battleship-microservices/player_service/src/main/java/org/example/player_service/controller/ShipController.java
+++ battleship_microservices/battleship_berkan_izgi/battleship-microservices/player_service/src/main/java/org/example/player_service/controller/ShipController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/ships")
 public class ShipController {
     private final ShipService shipService;
 
--- battleship_microservices/battleship_berkan_izgi/battleship-microservices/player_service/src/main/java/org/example/player_service/service/ShipService.java
+++ battleship_microservices/battleship_berkan_izgi/battleship-microservices/player_service/src/main/java/org/example/player_service/service/ShipService.java
@@ -1,5 +1,7 @@
 package org.example.player_service.service;
 
+import org.example.player_service.model.Player;
+import org.example.player_service.repository.PlayerRepository;
 import org.example.player_service.repository.ShipRepository;
 import org.springframework.stereotype.Service;
 import org.example.player_service.model.Ship;
@@ -9,9 +11,11 @@ import java.util.List;
 @Service
 public class ShipService {
     private final ShipRepository shipRepository;
+    private final PlayerRepository playerRepository;
 
-    public ShipService(ShipRepository shipRepository){
+    public ShipService(ShipRepository shipRepository, PlayerRepository playerRepository){
         this.shipRepository = shipRepository;
+        this.playerRepository = playerRepository;
     }
 
     public List<Ship> getShipsByPlayerId(Long playerId){
@@ -19,7 +23,12 @@ public class ShipService {
     }
 
     public Ship placeShip(Long playerId, int x, int y, int length, boolean isHorizontal){
+        Player player = playerRepository.findById(playerId).orElse(null);
+        if (player == null){
+            throw new IllegalArgumentException("Player mit der Id: " + playerId + "nicht gefunden ://");
+        }
         Ship ship = new Ship(x,y,length,isHorizontal);
+        ship.setPlayerId(playerId);
         return shipRepository.save(ship);
     }
 
